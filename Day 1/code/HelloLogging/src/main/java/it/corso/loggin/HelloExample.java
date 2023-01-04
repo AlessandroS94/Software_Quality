85c65be11aaa2c98049fa4738c24b6e056d978b9
AlessandroS94 <alessandro.sallese@student.univaq.it>
2023-01-04 09:06:43 +0100

    first documentation and example

---
+++ Day 1/code/HelloLogging/src/main/java/it/corso/loggin/HelloExample.java
@@ -0,0 +1,35 @@
+package it.corso.loggin;
+
+import org.apache.log4j.Logger;
+
+public class HelloExample{
+	
+	final static Logger logger = Logger.getLogger(HelloExample.class);
+	
+	public static void main(String[] args) {
+	
+		HelloExample obj = new HelloExample();
+		obj.tryPrintLog("Trying");
+		logger.warn("This is warn : " );
+		logger.error("This is error : " );
+		logger.fatal("This is fatal : " );
+		
+	}
+	
+	private void tryPrintLog(String parameter){
+		
+		if(logger.isDebugEnabled()){
+			logger.debug("This is debug : " + parameter);
+		}
+		
+		if(logger.isInfoEnabled()){
+			logger.info("This is info : " + parameter);
+		}
+		
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+		
+	}
+	
+}

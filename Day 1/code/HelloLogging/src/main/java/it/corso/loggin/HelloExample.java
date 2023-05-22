70ca003c40147c505933bf2f087538fbeef90b5d
AlessandroS94 <alessandro.sallese@student.univaq.it>
2023-05-22 11:55:19 +0200

    fix

--- Day 1/code/HelloLogging/src/main/java/it/corso/loggin/HelloExample.java
+++ Day 1/code/HelloLogging/src/main/java/it/corso/loggin/HelloExample.java
@@ -1,7 +1,8 @@
 package it.corso.loggin;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class HelloExample{
 	
--- Day 1/code/HelloLogging/src/main/java/it/corso/loggin/HelloExample2.java
+++ Day 1/code/HelloLogging/src/main/java/it/corso/loggin/HelloExample2.java
@@ -1,24 +1,20 @@
 package it.corso.loggin;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class HelloExample2{
 	
 	final static Logger logger = LogManager.getLogger(HelloExample2.class);
 	
 	public static void main(String[] args) {
-	
 		HelloExample2 obj = new HelloExample2();
-		
 		try{
 			obj.divide();
 		}catch(ArithmeticException ex){
 			logger.error("Sorry, something wrong!", ex);
 		}
-		
-		
 	}
 	
 	private void divide(){
